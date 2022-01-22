172fcdfcb84838d205c359d5a1ccaa50a4dc56ca
sachinkm308 <sachinkm308@gmail.com>
2022-01-22 15:12:23 +0100

    Added new files to test against tensorflow TF2.7 version

---
+++ Inference_OD_SS_TF2.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import tensorflow as tf
+from tensorflow.python.platform import gfile
+
+from ssd_encoder_decoder.ssd_output_decoder import decode_detections, decode_detections_fast
+
+
+import cv2
+import numpy as np
+
+from tensorflow.python.keras.backend import set_session
+graph = tf.compat.v1.get_default_graph()
+
+GRAPH_PB_PATH_OD='./frozen_model_od/tf_ssd7_model.pb'
+GRAPH_PB_PATH_FROZEN_SS='./frozen_model_ss/frozen_model_ss_plf.pb'
+
+
+tf_config = tf.compat.v1.ConfigProto()
+tf_config.gpu_options.allow_growth = False
+
+tf_sess1 = tf.compat.v1.Session(config=tf_config)
+tf_sess2 = tf.compat.v1.Session(config=tf_config)
+#loading the graph for OD
+
+
+with tf.compat.v1.Session() as sess1:
+   print("load graph_OD")
+   with gfile.FastGFile(GRAPH_PB_PATH_OD,'rb') as f:
+       graph_def1 = tf.compat.v1.GraphDef()
+   graph_def1.ParseFromString(f.read())
+   sess1.graph.as_default()
+   tf.import_graph_def(graph_def1, name='')
+   graph_nodes1=[n for n in graph_def1.node]
+   names = []
+   for t in graph_nodes1:
+      names.append(t.name)
+
+
+#loading the graph for SS
+with tf.compat.v1.Session() as sess2:
+   print("load graph_SS")
+   with gfile.FastGFile(GRAPH_PB_PATH_FROZEN_SS,'rb') as f:
+       graph_def2 = tf.compat.v1.GraphDef()
+   graph_def2.ParseFromString(f.read())
+   sess2.graph.as_default()
+   tf.import_graph_def(graph_def2, name='')
+   graph_nodes2=[n for n in graph_def2.node]
+   names = []
+   for t in graph_nodes2:
+      names.append(t.name)
+    # print operations
+
+   #                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            print(names)
+
+
+# Loading graphs sessions
+
+
+tf.import_graph_def(graph_def1, name='')
+tf.import_graph_def(graph_def2, name='')
+
+
+# Defining the inputs for the object detection graph
+
+tf_input1 = tf_sess1.graph.get_tensor_by_name('input_1:0')
+print(tf_input1)
+tf_predictions1 = tf_sess1.graph.get_tensor_by_name('predictions/concat:0')
+print(tf_predictions1)
+
+
+
+# Defining the inputs for the semantic segmentation graph
+tf_input2 = tf_sess2.graph.get_tensor_by_name('input_1:0')
+print('Tensor-2',tf_input2)
+
+tf_predictions2 = tf_sess2.graph.get_tensor_by_name('sigmoid/Sigmoid:0')
+print(tf_predictions2)
+
+
+## Drawing a bounding box around the predictions
+
+classes = ['background', 'car', 'truck', 'pedestrian', 'bicyclist', 'light'] # Just so we can print class names onto the image instead of IDs
+font = cv2.FONT_HERSHEY_SIMPLEX
+  
+# fontScale
+fontScale = 0.5
+
+# Blue color in BGR
+color = (255, 255, 0)
+
+# Line thickness of 2 px
+thickness = 1
+
+# functions for OD and SS
+
+def model_OS(image_resized2):
+    try:
+        with graph.as_default():
+            set_session(sess1)
+            inputs1, predictions1 = tf_sess1.run([tf_input1, tf_predictions1], feed_dict={
+            tf_input1: image_resized2[None, ...]
+        })
+
+        y_pred_decoded = decode_detections(predictions1,
+                                       confidence_thresh=0.5,
+                                       iou_threshold=0.45,
+                                       top_k=200,
+                                       normalize_coords=True,
+                                       img_height=300,
+                                       img_width=480)
+        np.set_printoptions(precision=2, suppress=True, linewidth=90)
+
+        for box in y_pred_decoded[0]:
+            xmin = box[-4]
+            ymin = box[-3]
+            xmax = box[-2]
+            ymax = box[-1]
+            label = '{}: {:.2f}'.format(classes[int(box[0])], box[1])
+            # cv2.rectangle(im2, (xmin,ymin),(xmax,ymax), color=color, thickness=2 )
+            cv2.rectangle(image_resized2, (int(xmin), int(ymin)), (int(xmax), int(ymax)), color=(0, 255, 0), thickness=2)
+            cv2.putText(image_resized2, label, (int(xmin), int(ymin)), font, fontScale, color, thickness)
+        return image_resized2
+
+
+    except:
+        print("Error in model_OS")
+
+def model_SS(image_resized3):
+    try:
+        with graph.as_default():
+            set_session(sess2)
+            inputs2, predictions2 = tf_sess2.run([tf_input2, tf_predictions2], feed_dict={
+            tf_input2: image_resized3[None, ...]
+        })
+        pred_image = 255*predictions2.squeeze()
+
+        ##converts pred_image to CV_8UC1 format so that ColorMap can be applied on it
+        u8 = pred_image.astype(np.uint8)
+
+        #Color map autumn is applied to the CV_8UC1 pred_image
+        im_color = cv2.applyColorMap(u8, cv2.COLORMAP_AUTUMN)
+        #cv2.imshow('input image', image_resized2)
+        #cv2.imshow('prediction mask',im_color)
+        return im_color
+
+
+    except:
+        print("Error in model_SS")
+
+
--- run_two_models.py
+++ run_two_models.py
@@ -1,5 +1,9 @@
 
-import Inference_OD_SS
+#Uncomment if you are using TF1
+#import Inference_OD_SS
+
+#Uncomment if you are using TF2
+import Inference_OD_SS_TF2
 import cv2
 
 image_os = cv2.imread("./1478899365487445082.jpg", cv2.IMREAD_UNCHANGED)
@@ -8,8 +12,13 @@ image_ss = cv2.imread("./0001TP_009060.png", cv2.IMREAD_UNCHANGED)
 image_os = cv2.resize(image_os, (480, 300))
 image_ss = cv2.resize(image_ss, (480, 320))
 
-a = Inference_OD_SS.model_OS(image_os)
-b = Inference_OD_SS.model_SS(image_ss)
+#Uncomment if you are using TF1
+#a = Inference_OD_SS.model_OS(image_os)
+#b = Inference_OD_SS.model_SS(image_ss)
+
+#Uncomment if you are using TF2
+a = Inference_OD_SS_TF2.model_OS(image_os)
+b = Inference_OD_SS_TF2.model_SS(image_ss)
 
 print(b)
 
